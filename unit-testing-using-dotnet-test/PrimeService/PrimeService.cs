baffa1abd71a7faddfba508e8f2832613170520f
elisama.santos <elisama.santos@ibge.gov.br>
2024-04-09 10:48:25 -0300

    criação do projeto de teste.

---
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -0,0 +1,14 @@
+﻿namespace PrimeService;
+
+using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
